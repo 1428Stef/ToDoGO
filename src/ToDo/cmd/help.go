10115dd47974b4fabc2232cd0d0d365fbc07ef53
Stef <romafon678@gmail.com>
2026-02-10 23:47:00 +0300

    fix: I forgot add edit in help

--- src/ToDo/cmd/help.go
+++ src/ToDo/cmd/help.go
@@ -6,6 +6,7 @@ func Help() {
     helpTxt :=  "1. add - create a new task \n" +
 "2. done - mark a task as completed \n"+
 "3. del - delete a task \n"+
-"4. list - display the list of tasks "
+"4. list - display the list of tasks \n"+
+"5. edit - changes the task title"
     fmt.Println(helpTxt)
 }
