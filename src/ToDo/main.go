e83b8c1dd69955d704016b1db5a3a3a389ebb8dd
Stef <romafon678@gmail.com>
2026-03-27 12:30:49 +0300

    fix: add error handling

--- src/ToDo/main.go
+++ src/ToDo/main.go
@@ -19,30 +19,45 @@ func main() {
 		title := addCmd.String("title", "", "")
 		addCmd.Parse(os.Args[2:])
 		if *title == "" {
+			fmt.Fprintln(os.Stderr, "error: --title is required")
 			os.Exit(1)
 		}
 		
-		cmd.Add(*title)
+		if err := cmd.Add(*title); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "list":
-		cmd.List()
+		if err := cmd.List(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "done":
 		doneCmd := flag.NewFlagSet("done",flag.ExitOnError)
 		id := doneCmd.Int("id", 0, "")
 		doneCmd.Parse(os.Args[2:]) 
 		if *id == 0 {
+			fmt.Fprintln(os.Stderr, "error: --id is required")
 			os.Exit(1)
 		}
 
-		cmd.Done(*id)
+		if err := cmd.Done(*id); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "del":
 		delCmd := flag.NewFlagSet("del", flag.ExitOnError)
 		id := delCmd.Int("id", 0, "")
 		delCmd.Parse(os.Args[2:])
 		if *id == 0 {
+			fmt.Fprintln(os.Stderr, "error: --id is required")
 			os.Exit(1)
 		}
 
-		cmd.Del(*id)
+		if err := cmd.Del(*id); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "help":
 		cmd.Help()
 	case "edit":
@@ -51,12 +66,17 @@ func main() {
 		title := editCmd.String("title", "", "")
 		editCmd.Parse(os.Args[2:])
 		if *id == 0 {
+			fmt.Fprintln(os.Stderr, "error: --id is required")
 			os.Exit(1)
 		}
 		if *title == "" {
+			fmt.Fprintln(os.Stderr, "error: --title is required")
 			os.Exit(1)
 		}
-		cmd.Edit(*id, *title)
+		if err := cmd.Edit(*id, *title); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	default:
 		fmt.Printf("Unknown command: %s \n", os.Args[1])
 		os.Exit(1)
